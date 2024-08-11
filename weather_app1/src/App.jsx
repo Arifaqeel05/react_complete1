4d7e0bf27f1af3b95278c83bc782a9ef1fa444fb
ARIF AQEEL AHMAD <arifaqeelahmad059@gmail.com>
2024-08-11 12:57:53 +0500

    weather_app1

---
+++ weather_app1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Search from './componet/Search'
+import Result from './componet/Result'
+
+
+function App() {
+
+  return (
+    <div>
+      <Search/>
+      <Result/>
+    </div>
+      
+    
+  )
+}
+
+export default App
